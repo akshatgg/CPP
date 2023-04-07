8e008dc37f6e4b8850290bd83ced8f44989ab91d
akshatgg <akshatg9636@gmail.com>
2023-04-07 23:10:54 +0530

    leet code

---
+++ leetcode.cpp
@@ -0,0 +1,31 @@
+#include <cmath>
+#include <cstdio>
+#include<string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <cassert>
+using namespace std;
+int main()
+{
+    int n,num[100];
+    cin>>n;
+
+    for(int i=1;i<=n;i++)
+    {
+        cin>>num[i];
+    }
+
+    for (int i=1;i<=2*n;i++)
+    {
+        if(i<=n)
+        {
+            cout<<num[i]<<" ";
+        }
+        else (i>n);
+        {
+            cout<<num[i-n]<<" ";
+        }
+    }
+    return 0;
+}
